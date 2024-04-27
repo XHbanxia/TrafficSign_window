b7050b28cd1bbf7cac07b7e950b6e5d43dcc1cdc
XHbanxia <73838235+XHbanxia@users.noreply.github.com>
2024-04-27 18:14:46 +0800

    add global image path and callback function

---
+++ UI/ImageSeg/imagecut.py
@@ -0,0 +1,34 @@
+from PIL import Image
+from Model.detect_model.my_yolo import myyolo
+
+def cutimge(imagepath,xywh):
+    image = Image.open(imagepath)
+    cropped_images = []
+
+    xywh = xywh.tolist()
+    # 遍历列表中的每个框
+    for x, y, w, h in xywh:
+        # 计算裁剪框的左上角和右下角坐标
+        left = x - w // 2
+        top = y - h // 2
+        right = x + w // 2
+        bottom = y + h // 2
+
+        # print(left, top, right, bottom)
+        # 裁剪图片
+        cropped_image = image.crop((left, top, right, bottom))
+        cropped_images.append(cropped_image)
+
+    # 返回裁剪后的图片列表
+    return cropped_images
+
+
+if __name__=="__main__":
+    img_path = r"E:\TranfficSign\ObjectCheck\tt100k_2021\test\0000002.jpg"
+
+    xywh = myyolo.prodectfunc(img_path)
+    # for item in xywh:
+    #     print(item)
+    crop_image = cutimge(img_path, xywh)
+    for item in crop_image:
+        item.show()
